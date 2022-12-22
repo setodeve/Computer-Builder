e6e30ef97acfceca6bedd59f67e8f0bedc7ea64f
setodeve <keinick11@outlook.com>
2022-12-22 09:43:14 +0900

    add comment

--- src/components/DefaultLayout.vue
+++ src/components/DefaultLayout.vue
@@ -5,7 +5,7 @@
         <div>step1:Select your CPU</div>
         <div id="select">
           Brand
-          <select id="selectBrand">          
+          <select id="selectBrand" v-on:change="setBrandData">          
             <option value="-">-</option>
             <option v-for="(data, index) in store.trimData(store.$state.fetchdata,'Brand')" :key=index v-bind:value="data['Brand']">
               {{ data['Brand'] }}
@@ -13,9 +13,9 @@
           </select>
 
           Model 
-          <select name="" id="">
+          <select name="" id="" v-on:change="setModelData">
             <option value="-">-</option>
-            <option v-for="(data, index) in store.trimData(store.$state.fetchdata,'Model')" :key=index v-bind:value="data['Model']" >
+            <option v-for="(data, index) in store.trimData(store.extractBrandData(store.$state.fetchdata),'Model')" :key=index v-bind:value="data['Model']" >
               {{ data['Model'] }}
             </option>
           </select>        
@@ -32,6 +32,14 @@ import { cpuStore } from '../stores/cpu'
 
 const store = cpuStore();
 store.getData("https://api.recursionist.io/builder/computers?type=cpu")
+
+function setBrandData(e){
+  store.$state.brand = e.target.value ;
+}
+
+function setModelData(e){
+  store.$state.model =e.target.value ;
+}
 
 </script>
 
--- src/stores/cpu.ts
+++ src/stores/cpu.ts
@@ -2,27 +2,41 @@ import { defineStore } from 'pinia'
 
 export const cpuStore = defineStore('cpu',{
   state: () => ({
-    brand: "test",
-    model: "",
+    brand: "-",
+    model: "-",
     fetchdata: {},
     Loading:false
   }),
   actions:{
-    setData(brand:string, model:string){
-      this.brand = brand
-      this.model = model
-    },
+    /**
+     * urlをfetchし、fetchしたデータをセットする関数
+     * @param {string} url fetch先のURL
+     */
     getData(url:string){
-      return fetch(url)
+      fetch(url)
       .then(data => data.json())
       .then(data => this.fetchdata = data)
       .then(() => this.Loading = true )
     },
-    trimData(array,str){
+    /**
+     * strの要素だけデータをトリミングする関数
+     * @param {any} array 配列データ
+     * @param {string} str BrandやModelなどの文字列
+     * @return {stirng} トリミングした配列
+     */
+    trimData(array:any,str:string){
       return array.filter((item, index, self) => {
         const nameList = self.map(item => item[str]);
         if (nameList.indexOf(item[str]) === index) return item;
       })
+    },
+    /**
+     * Brandで選択されたの要素だけデータを抽出する関数
+     * @param {any} array 配列データ
+     * @return {stirng} 抽出した配列
+     */
+    extractBrandData(array:any){
+      return array.filter(w=>w.Brand==this.$state.brand)
     }
-  }
+  },
 })
