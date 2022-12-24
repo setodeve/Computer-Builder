6d5ed9dd7192adba4ca0dfc25c83ffa2b7368dbf
setodeve <keinick11@outlook.com>
2022-12-24 09:30:34 +0900

    add component

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,11 @@
 <script setup lang="ts">
   import { storeToRefs } from "pinia";
+  import Header from "./components/Header.vue" ;
   import CPU_GPU from "./components/Contents/CPU_GPU.vue" ;
   import RAM from "./components/Contents/RAM.vue" ;
   import STORAGE from "./components/Contents/STORAGE.vue" ;
+  import Button from "./components/Button.vue" ;
+  import Result from "./components/Result.vue" ;
   import { cpuStore } from "./stores/cpu";
   import { gpuStore } from "./stores/gpu";
   import { ramStore } from "./stores/ram";
@@ -15,6 +18,7 @@
 </script>
 
 <template>
+    <Header></Header>
     <CPU_GPU type="CPU"></CPU_GPU>
     <CPU_GPU type="GPU"></CPU_GPU>
     <RAM></RAM>
@@ -26,6 +30,8 @@
     {{ ramdata }}
     <br>
     {{ storagedata }}
+    <Button></Button>
+    <Result></Result>
 </template>
 
 <style scoped>
