0b54d546f7d542147c004a4b14f5581f6b3b399a
setodeve <keinick11@outlook.com>
2022-12-25 18:11:40 +0900

    edit result component

--- src/components/Result.vue
+++ src/components/Result.vue
@@ -1,26 +1,47 @@
 <template>
-  {{ data }}
+  <table>
+  <tr>
+    <td>CPU</td>
+    <td>{{ data[0].Model}}</td>
+  </tr>
+  <tr>
+    <td>GPU</td>
+    <td>{{ data[1].Model}}</td>
+  </tr>
+  <tr>
+    <td>RAM</td>
+    <td>{{ data[2].Model}}</td>
+  </tr>
+  <tr>
+    <td>STORAGE</td>
+    <td>{{ data[3].Model}}</td>
+  </tr>
+</table>
+  <div>
+    <span>
+      Gaming : {{ gamingResult.toFixed() }}%
+      Work :{{ homeResult.toFixed() }}%
+    </span>
+  </div>
+
 </template>
 
 <script>
 import { defineComponent } from "vue";
 export default defineComponent({
- props:{
-    data: Array
- },
- created(){
-  //   const gamingResult = data[0].Benchmark*0.6 +data[1].Benchmark*0.25 
-  //                 + data[2].Benchmark*0.125 + data[3].Benchmark*0.025 ;
-  //   const homeResult = data[0].Benchmark*0.6 +data[1].Benchmark*0.25 
-  //                 + data[2].Benchmark*0.1 + data[3].Benchmark*0.05 ;
-  // console.log(gamingResult);
-  // console.log(homeResult);
- },
- watch: {
-  // forceUpdateMyComponent() {
-  //     this.$forceUpdate()
-  // },
- }
+  data() {
+    const gamingResult = this.data[0].Benchmark*0.6 + this.data[1].Benchmark*0.25 
+                    + this.data[2].Benchmark*0.125 + this.data[3].Benchmark*0.025 ;
+    const homeResult = this.data[0].Benchmark*0.6 +this.data[1].Benchmark*0.25 
+                    + this.data[2].Benchmark*0.1 + this.data[3].Benchmark*0.05 ;
+    return {
+      gamingResult,
+      homeResult
+    }
+  },
+  props:{
+      data: Array
+  }
 })
 </script>
 
