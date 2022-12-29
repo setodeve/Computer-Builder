81ed16f99a6fa7b37ec3c6aca233e7bf7214025c
setodeve <keinick11@outlook.com>
2022-12-29 17:59:18 +0900

    store test #8

--- src/test/cpuStore.test.ts
+++ src/test/cpuStore.test.ts
@@ -6,6 +6,7 @@ describe('Counter Store', () => {
   beforeEach(() => {
     setActivePinia(createPinia())
   })
+
   test('cpuSore.brand is - ?', () => {
     const store = cpuStore() ;
     expect(store.$state.brand).toBe('-') ;
@@ -18,4 +19,31 @@ describe('Counter Store', () => {
     expect(store.$state.Loading).toBeTruthy() ;
   })
   
+  test('trim data', async() => {
+    const store = cpuStore() ;
+    await store.getData("https://api.recursionist.io/builder/computers?type=cpu");
+    expect(store.$state.fetchdata).toHaveLength(100) ;
+    const rtnArray = store.trimData(store.$state.fetchdata,"Brand");
+    expect(rtnArray).toHaveLength(2) ;
+  })
+
+  test('extract data cpu', async() => {
+    const store = cpuStore() ;
+    await store.getData("https://api.recursionist.io/builder/computers?type=cpu");
+    expect(store.$state.fetchdata).toHaveLength(100) ;
+    store.$state.brand = "Intel";
+    const rtnArray = store.extractBrandData(store.$state.fetchdata);
+    expect(rtnArray).toHaveLength(79) ;
+  })
+
+  test('set data', () => {
+    const store = cpuStore() ;
+    const eventInput = {
+      target:{
+        value:"Intel"
+      }
+    }
+    store.setBrandData(eventInput);
+    expect(store.$state.brand).toBe("Intel") ;
+  })
 })
