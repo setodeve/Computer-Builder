380222ef65abd2b741fc39ad20b2e3005f0dc3db
setodeve <keinick11@outlook.com>
2022-12-30 13:26:36 +0900

    change vite config

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: process.env.NODE_ENV === 'production' ? '/github-pages-test/' : './',
   plugins: [vue()],
   test:{
     environment: 'jsdom'
